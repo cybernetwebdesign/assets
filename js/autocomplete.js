3959696684f17775ea4d8538fd79d243af2d432e
Cybernet Web Design <cybernetwebdesign@gmail.com>
2019-09-27 00:00:39 +0700

    Add files via upload

---
+++ js/autocomplete.js
@@ -0,0 +1,22 @@
+        <script>
+    		$("#youtube").autocomplete({
+    			source: function(request, response){
+    				var query = request.term;
+    				$.ajax({
+    					url: "<?php echo $site_url;?>/autocomplete.php?q="+query,  
+    					dataType: 'jsonp',
+    					success: function(data, textStatus, request) { 
+    					   response( $.map( data[1], function(item) {
+    							return {
+    								label: item[0],
+    								value: item[0]
+    							}
+    						}));
+    					}
+    				});
+    			},
+              select: function(event, ui) {
+                    window.location.href = 'https://youtube.download-lagu-mp3.com/search/' + encodeURI(ui.item.value).replace(/%20/g, '-')
+                }
+    		});
+        </script>
